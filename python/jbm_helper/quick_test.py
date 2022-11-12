bdcb99fa636111e22b066b7fa7704507662a208b
c-c-k <cck-github@nym.hush.com>
2022-11-13 01:56:23 +0200

    quick_test.py upgrade.

--- python/jbm_helper/quick_test.py
+++ python/jbm_helper/quick_test.py
@@ -13,53 +13,123 @@ import typing
 
 
 class QuickTest:
-    __slots__ = ["functions", "mode", "suppressed_exceptions"]
+    __slots__ = ["functions", "mode", "suppressed_exceptions",
+                 "args", "kwargs", "inputs", "sysargv", "sysargv_orig"]
 
     def __init__(self):
         self.functions: list[typing.Callable] | None = None
-        self.mode: str | None = "input"
-        self.suppressed_exceptions: list[Exception] = []
+        self.mode: str | None = "input"  # currently unused.
+        self.suppressed_exceptions: list[Exception] = list()
+        self.inputs: tuple = tuple()
+        self.sysargv: list | None = None
+        self.sysargv_orig: list = sys.argv
+        self.args: tuple = tuple()
+        self.kwargs: dict = dict()
 
-    def test(self, *args):
+    def test_functions(
+            self, *_inputs_only,
+            inputs: tuple | None = None,
+            sysargv: list | None = None,
+            args: tuple | None = None,
+            kwargs: dict | None = None,
+            suppressed_exceptions: tuple | None = None,
+    ):
         if not self.functions:
             return
-        if self.mode == "input":
-            for function in self.functions:
-                self.test_inputs_for_function(function, *args)
+        self.inputs = inputs if inputs else _inputs_only
+        self.sysargv = sysargv if sysargv else None
+        self.args = args if args else tuple()
+        self.kwargs = kwargs if kwargs else dict()
+        self.suppressed_exceptions = suppressed_exceptions
+        for function in self.functions:
+            self._test_function(function)
 
-    def test_inputs_for_function(self, function: typing.Callable, *inputs):
-        print(f"== testing function {function.__name__} with inputs: {inputs} ==")
-        has_data = bool(self.mode)
-        inputs = (str(input_) + "\n" for input_ in inputs)
-
-        def stdin_readline_override() -> str:
-            next_input = next(inputs)
-            print(next_input, end="")
-            return next_input
-
-        if has_data:
-            sys.stdin.readline_ = sys.stdin.readline
-            sys.stdin.readline = stdin_readline_override
+    def _test_function(self, function: typing.Callable):
+        self._print_test_info_message(function)
+        self._override_sysargv()
+        self._override_stdin()
         try:
-            function()
+            function(*self.args, **self.kwargs)
         except Exception as error:
-            if type(error) in self.suppressed_exceptions:
-                print(type(error), *error.args, sep=" : ")
-            else:
-                raise
-        if has_data:
-            sys.stdin.readline = sys.stdin.readline_
+            self._handle_suppress_exceptions(error)
+        self._restore_stdin()
+        self._restore_sysargv()
+
+    def _print_test_info_message(self, function: typing.Callable):
+        print(f"== testing function {function.__name__}", end="; ")
+        if self.args:
+            print(f"args: {self.args}", end="; ")
+        if self.kwargs:
+            print(f"kwargs: {self.kwargs}", end="; ")
+        if self.sysargv:
+            print(f"sysargv: {self.sysargv}", end="; ")
+        if self.inputs:
+            print(f"inputs: {self.inputs}", end="; ")
+        if self.suppressed_exceptions:
+            print("suppressing exceptions: "
+                  f"{self.suppressed_exceptions}", end="; ")
+        print("==")
+
+    def _handle_suppress_exceptions(self, error: Exception):
+        if type(error) in self.suppressed_exceptions:
+            print(type(error), *error.args, sep=" : ")
+        else:
+            raise
+
+    def _override_stdin(self):
+        sys.stdin = StdinOverride(self.inputs)
+
+    @staticmethod
+    def _restore_stdin():
+        sys.stdin = sys.__stdin__
+
+    def _override_sysargv(self):
+        if self.sysargv is not None:
+            sys.argv = sys.argv[0:1]
+            sys.argv.extend(self.sysargv)
+
+    def _restore_sysargv(self):
+        sys.argv = self.sysargv_orig
+
+
+class StdinOverride:
+    __slots__ = ["inputs"]
+
+    def __init__(self, inputs: tuple):
+        self.inputs = (str(input_) + "\n" for input_ in inputs)
+
+    def readline(self) -> str:
+        next_input = next(self.inputs)
+        print(next_input, end="")
+        return next_input
 
 
 # ------------------------------------------------------------
 # FUNCTIONS
 # ------------------------------------------------------------
-def main():
-    pass
+def main(arg1, arg2, *args, **kwargs):
+    print(arg1, arg2)
+    print(args, kwargs)
+    print(sys.argv)
+    input("input: ")
+    raise ValueError
+
+
+def quicktest_main():
+    tester = QuickTest()
+    tester.functions = [main]
+    # Test helper.
+    tester.test_functions(
+        args=(1,),
+        kwargs={"arg2": 3, "arg4": 4},
+        inputs=(5, 6),
+        sysargv=[7, 8],
+        suppressed_exceptions=(ValueError,)
+    )
 
 
 # ------------------------------------------------------------
 # PROGRAM CODE
 # ------------------------------------------------------------
 if __name__ == "__main__":
-    main()
+    quicktest_main()
