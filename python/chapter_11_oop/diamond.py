767e075365738dede545d19a8a05feeaf4129401
c-c-k <cck-github@nym.hush.com>
2022-11-23 20:50:09 +0200

    Chapter 11 questions (work in progress).

--- python/chapter_11_oop/diamond.py
+++ python/chapter_11_oop/diamond.py
@@ -7,71 +7,68 @@
 # IMPORTS
 # ------------------------------------------------------------
 # -=- ANSWER START -=-
-import enum
+CUT_STR_TO_INT = {
+    "ideal": 5,
+    "premium": 4,
+    "very_good": 3,
+    "good": 2,
+    "fair": 1,
+    "unknown": 0,
+}
+CLARITY_STR_TO_INT = {
+    "fl": 11,
+    "if": 10,
+    "vvs1": 9,
+    "vvs2": 8,
+    "vs1": 7,
+    "vs2": 6,
+    "si1": 5,
+    "si2": 4,
+    "i1": 3,
+    "i2": 2,
+    "i3": 1,
+    "unknown": 0,
+}
 
-
-class QualityEnum(enum.Enum):
-    def __eq__(self, other: "QualityEnum"):
-        if self.__class__ is other.__class__:
-            return self.value == other.value
-        else:
-            return NotImplemented
-
-    def __ge__(self, other: "QualityEnum"):
-        if self.__class__ is other.__class__:
-            return self.value >= other.value
-        else:
-            return NotImplemented
-
-    def __gt__(self, other: "QualityEnum"):
-        if self.__class__ is other.__class__:
-            return self.value > other.value
-        else:
-            return NotImplemented
-
-    def __le__(self, other: "QualityEnum"):
-        if self.__class__ is other.__class__:
-            return self.value <= other.value
-        else:
-            return NotImplemented
-
-    def __lt__(self, other: "QualityEnum"):
-        if self.__class__ is other.__class__:
-            return self.value < other.value
-        else:
-            return NotImplemented
-
-
-
-
-class Cut(enum.IntEnum):
-    FAIR = 1
-    GOOD = 2
-    VERY_GOOD = 3
-    PREMIUM = 4
-    IDEAL = 5
-
-    @staticmethod
-    def get_cut_from_str(cut: str) -> "Cut":
-        str_to_enum = {
-            "FAIR": Cut.FAIR,
-            "GOOD": Cut.GOOD,
-            "VERY_GOOD": Cut.VERY_GOOD,
-            "PREMIUM": Cut.PREMIUM,
-            "IDEAL": Cut.IDEAL,
-        }
+BasicValue = int | float
+GradedValue = int | str
 
 
 class Diamond:
-    __slots__ = (
-        "carat", "cut", "color", "clarity", "depth", "table", "price",
-        "x", "y", "z",
-    )
-    
+    cut_str_to_int: dict = {
+        grade_name: grade_id for grade_name, grade_id in CUT_STR_TO_INT.items()}
+    cut_int_to_str: dict = {
+        grade_id: grade_name for grade_name, grade_id in CUT_STR_TO_INT.items()}
+    clarity_str_to_int: dict = {
+        grade_name: grade_id for grade_name, grade_id in CLARITY_STR_TO_INT.items()}
+    clarity_int_to_str: dict = {
+        grade_id: grade_name for grade_name, grade_id in CLARITY_STR_TO_INT.items()}
+    _graded_values: dict = {
+        "cut": {
+            "str_to_int": cut_str_to_int,
+            "int_to_str": cut_int_to_str
+        },
+        "clarity": {
+            "str_to_int": clarity_str_to_int,
+            "int_to_str": clarity_int_to_str
+        }
+    }
+    _carat: float = None
+    _cut: int = None
+    _color: str = None
+    _clarity: int = None
+    _depth: float = None
+    _table: float = None
+    _price: float = None
+    _x: float = None
+    _y: float = None
+    _z: float = None
+
     def __init__(
-            self,
-            carat: float, cut: str, color: str, clarity: str, depth: float,
-            table: float, price: float, x: float, y: float, z: float,
+            self, carat: float = 0.0, cut: str = "",
+            color: str = "", clarity: str = "", depth: float = 0.0,
+            table: float = 0.0, price: float = 0.0,
+            x: float = 0.0, y: float = 0.0, z: float = 0.0,
     ):
         self.carat = carat
         self.cut = cut
@@ -83,9 +80,134 @@ class Diamond:
         self.x = x
         self.y = y
         self.z = z
+        
+    def __str__(self):
+        return (f"carat: {self.carat}, cut: {self.cut}, color: {self.color},"
+                f" clarity: {self.clarity}, depth: {self.depth},"
+                f" table: {self.table}, price: {self.price},"
+                f" x: {self.x}, y: {self.y}, z: {self.z},")
+
+    def _set_basic_value(self, field_name: str, value: BasicValue):
+        self.__setattr__("_" + field_name, value)
+
+    def _get_basic_value(self, field_name: str):
+        return self.__getattribute__("_" + field_name)
+
+    def _set_graded_value(self, field_name: str, grade: GradedValue, grade_map: dict):
+        if isinstance(grade, str):
+            try:
+                grade = grade_map[grade.lower()]
+            except KeyError:
+                grade = 0
+        elif isinstance(grade, int):
+            if grade not in grade_map.values():
+                grade = 0
+        else:
+            grade = 0
+        self.__setattr__("_" + field_name, grade)
+
+    def _get_graded_value(self, field_name: str, grade_map: dict):
+        grade = self.__getattribute__("_" + field_name)
+        return grade_map[grade]
 
     def get(self, field_name: str):
-        return self.__getattribute__(field_name)
+        if field_name in self._graded_values:
+            return self._get_graded_value(
+                field_name,
+                self._graded_values[field_name]["int_to_str"]
+            )
+        else:
+            return self._get_basic_value(field_name)
+
+    def set(self, field_name: str, value: BasicValue | GradedValue):
+        if field_name in self._graded_values:
+            self._set_graded_value(
+                field_name,
+                value,
+                self._graded_values[field_name]["str_to_int"]
+            )
+        else:
+            self._set_basic_value(field_name, value)
+
+    @property
+    def carat(self):
+        return self.get("carat")
+
+    @carat.setter
+    def carat(self, value):
+        self.set("carat", value)
+
+    @property
+    def cut(self):
+        return self.get("cut")
+
+    @cut.setter
+    def cut(self, value):
+        self.set("cut", value)
+
+    @property
+    def color(self):
+        return self.get("color")
+
+    @color.setter
+    def color(self, value):
+        self.set("color", value)
+
+    @property
+    def clarity(self):
+        return self.get("clarity")
+
+    @clarity.setter
+    def clarity(self, value):
+        self.set("clarity", value)
+
+    @property
+    def depth(self):
+        return self.get("depth")
+
+    @depth.setter
+    def depth(self, value):
+        self.set("depth", value)
+
+    @property
+    def table(self):
+        return self.get("table")
+
+    @table.setter
+    def table(self, value):
+        self.set("table", value)
+
+    @property
+    def price(self):
+        return self.get("price")
+
+    @price.setter
+    def price(self, value):
+        self.set("price", value)
+
+    @property
+    def x(self):
+        return self.get("x")
+
+    @x.setter
+    def x(self, value):
+        self.set("x", value)
+
+    @property
+    def y(self):
+        return self.get("y")
+
+    @y.setter
+    def y(self, value):
+        self.set("y", value)
+
+    @property
+    def z(self):
+        return self.get("z")
+
+    @z.setter
+    def z(self, value):
+        self.set("z", value)
 
 
 class DiamondList:
@@ -124,9 +246,6 @@ class DiamondList:
     def get_highest_carat(self) -> "DiamondList":
         return self.__get_highest("carat")
 
-    def get_higher_than(self) -> "DiamondList":
-        return self.__get_highest("price")
-
 
 # -=- ANSWER END -=-
 # -=- TEST START -=-
@@ -135,6 +254,67 @@ class DiamondList:
 # ------------------------------------------------------------
 # --UNUSED
 # ------------------------------------------------------------
+# class Diamond:
+# __slots__ = (
+#     "carat", "cut", "color", "clarity", "depth", "table", "price",
+#     "x", "y", "z",
+# )
+
+# import enum
+# 
+# 
+# class QualityEnum(enum.Enum):
+#     def __eq__(self, other: "QualityEnum"):
+#         if self.__class__ is other.__class__:
+#             return self.value == other.value
+#         else:
+#             return NotImplemented
+# 
+#     def __ge__(self, other: "QualityEnum"):
+#         if self.__class__ is other.__class__:
+#             return self.value >= other.value
+#         else:
+#             return NotImplemented
+# 
+#     def __gt__(self, other: "QualityEnum"):
+#         if self.__class__ is other.__class__:
+#             return self.value > other.value
+#         else:
+#             return NotImplemented
+# 
+#     def __le__(self, other: "QualityEnum"):
+#         if self.__class__ is other.__class__:
+#             return self.value <= other.value
+#         else:
+#             return NotImplemented
+# 
+#     def __lt__(self, other: "QualityEnum"):
+#         if self.__class__ is other.__class__:
+#             return self.value < other.value
+#         else:
+#             return NotImplemented
+# 
+# 
+# 
+# 
+# class Cut(enum.IntEnum):
+#     FAIR = 1
+#     GOOD = 2
+#     VERY_GOOD = 3
+#     PREMIUM = 4
+#     IDEAL = 5
+# 
+#     @staticmethod
+#     def get_cut_from_str(cut: str) -> "Cut":
+#         str_to_enum = {
+#             "FAIR": Cut.FAIR,
+#             "GOOD": Cut.GOOD,
+#             "VERY_GOOD": Cut.VERY_GOOD,
+#             "PREMIUM": Cut.PREMIUM,
+#             "IDEAL": Cut.IDEAL,
+#         }
+
+
 def main():
     print("This module is not meant to be run as main.")
 
--- python/chapter_11_oop/question_01.py
+++ python/chapter_11_oop/question_01.py
@@ -7,7 +7,7 @@
 # IMPORTS
 # ------------------------------------------------------------
 from python.jbm_helper.quick_test import QuickTest
-from diamond import Diamond
+from diamond import Diamond, DiamondList
 import csv
 
 
@@ -21,13 +21,20 @@ def answer_01():
 def quicktest_answers():
     tester = QuickTest()
     tester.functions = [answer_01]
-    diamond = Diamond(0.23, "Ideal", "E", "SI2", 61.5, 55.0, 326, 3.95, 3.98, 2.43)
-    with open("diamonds.csv", "r", newline='') as f:
+    # diamond = Diamond(0.23, "Ideal", "E", "SI2", 61.5, 55.0, 326, 3.95, 3.98, 2.43)
+    diamond_list = DiamondList()
+    with open("diamonds_test.csv", "r", newline='') as f:
         diamonds = csv.DictReader(f)
-        cut_names = set()
         for diamond in diamonds:
-            cut_names.add(diamond["cut"])
-    print(cut_names)
+            diamond_list.add(Diamond(**diamond))
+    print(*diamond_list.get_all(), sep="\n")
+
+    # with open("diamonds.csv", "r", newline='') as f:
+    #     diamonds = csv.DictReader(f)
+    #     cut_names = set()
+    #     for diamond in diamonds:
+    #         cut_names.add(diamond["clarity"])
+    # print(cut_names)
 
 
 # ------------------------------------------------------------
